416a440374ecf7ddd2fac644641f33011700d105
Angelo Zammataro <juba.kem@gmail.com>
2022-07-30 15:01:33 +0200

    Primi passi - First API - 03 FIX a BUG

--- src/main/java/it2/develhope/firstAPI03/controllers/StringController.java
+++ src/main/java/it2/develhope/firstAPI03/controllers/StringController.java
@@ -1,18 +1,16 @@
 package it2.develhope.firstAPI03.controllers;
 
 import it2.develhope.firstAPI03.DTO.StringsDTO;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @RestController
 public class StringController {
 
-    @PostMapping(value = "/strings")
+    @GetMapping(value = "/strings")
     public String strings(@Valid @RequestBody StringsDTO stringsDTO){
         //return stringsDTO.toString();
         return stringsDTO.getStringMandatory() + " " + stringsDTO.getStringOptional();
